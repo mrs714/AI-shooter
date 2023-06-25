31261f80fcff3ac4d4ca07059b1540821f12f3d4
Miquel Rodríguez Sansaloni <miquel.rodriguez.sansaloni@estudiantat.upc.edu>
2023-06-25 13:28:08 +0200

    Refactored NeuralNetwork

--- AI Shooter/Assets/Scripts/NeuralNetwork/NeuralNetwork.cs
+++ AI Shooter/Assets/Scripts/NeuralNetwork/NeuralNetwork.cs
@@ -39,33 +39,36 @@ public class NeuralNetwork
         this.hiddenLayersNumber = HiddenLayersNumber;
         this.neuronsPerLayer = NeuronsPerLayer;
 
+        Initialize();
+    }
+
+    // Methods: 
+
+    // Initialize the network
+    public void Initialize() {
         layers = new Layer[hiddenLayersNumber + 2];
 
         //create layers
-        for (int i = 0; i < layers.Length; i++)
-        {
+        for (int i = 0; i < layers.Length; i++) {
             layers[i] = new Layer();
         }
 
         //populate them
         for (int i = 0; i < layers.Length; i++) {
             //Create ammount of neurons depending on the layer:
-            //If first layer:
-            if (i == 0) 
-            {
+            if (i == 0) {
                 layers[i].neurons = new Neuron[firstLayerNumber];
             }
-            else if (i == hiddenLayersNumber + 1) 
-            {
+            else if (i == hiddenLayersNumber + 1) {
                 layers[i].neurons = new Neuron[lastLayerNumber];
             }
             else {
-            layers[i].neurons = new Neuron[neuronsPerLayer];
+                layers[i].neurons = new Neuron[neuronsPerLayer];
             }
-            
+
             //populate neurons, the layer before the last has a different number of weights, the last one has none:
             for (int j = 0; j < layers[i].neurons.Length; j++) {
-                int numberOfWeights =  i != hiddenLayersNumber ? neuronsPerLayer : lastLayerNumber;
+                int numberOfWeights = i != hiddenLayersNumber ? neuronsPerLayer : lastLayerNumber;
                 float[] weights = new float[numberOfWeights];
                 for (int k = 0; k < numberOfWeights; k++) {
                     weights[k] = Random.Range(-1f, 1f);
@@ -75,9 +78,7 @@ public class NeuralNetwork
         }
     }
 
-    //Methods: 
-
-    //clear neuron values
+    // Clear neuron values
     public void ClearValues() {
         for (int i = 0; i < layers.Length; i++) {
             for (int j = 0; j < layers[i].neurons.Length; j++) {
@@ -86,51 +87,52 @@ public class NeuralNetwork
         }
     }
 
-    //evolve the network
+    // Evolve the network
     public void Mutate() {
-        //for each layer:
+        // For each layer:
         for (int i = 0; i < layers.Length; i++) {
-            //for each neuron:
+            // For each neuron:
             for (int j = 0; j < layers[i].neurons.Length; j++) {
-                //for each weight:
+                // For each weight:
                 for (int k = 0; k < layers[i].neurons[j].weights.Length; k++) {
-                    //mutate the weight:
+                    // Mutate the weight:
                     layers[i].neurons[j].weights[k] += Random.Range(-0.1f, 0.1f);
                 }
-                //mutate the bias:
+                // And the bias:
                 layers[i].neurons[j].bias += Random.Range(-0.1f, 0.1f);
             }
         }
     }
 
-    //calculate values
+    // Calculate values
     public float[] Calculate(float[] inputs)
     {
         ClearValues();
-        //assign weights to first layer:
+        // Assign weights to first layer:
         for (int i = 0; i < firstLayerNumber; i++) {
             layers[0].neurons[i].value = inputs[i];
         }
-        //for each hidden layer, and the last one:
+
+        // For each hidden layer, and the last one:
         for (int i = 1; i <= hiddenLayersNumber + 1; i++)
         {
-            //for each neuron in the layer:
+            // For each neuron in the layer:
             for (int j = 0; j < layers[i].neurons.Length; j++)
             {
-                //We take the previous layer and calculate the value of the neuron:
-                Layer layer = layers[i - 1];
-                foreach (Neuron neuron in layer.neurons)
+                // We take the previous layer and calculate the value of the neuron:
+                foreach (Neuron neuron in layers[i - 1].neurons)
                 {
                     layers[i].neurons[j].value += Mathf.Clamp((neuron.value * neuron.weights[j]) + neuron.bias, -1, 1) ;
                 }
             }
         }
 
-        //put the values of the last layer into a float array:
+        // Put the values of the last layer into a float array:
         float[] outputs = new float[lastLayerNumber];
         for (int i = 0; i < lastLayerNumber; i++) {
             outputs[i] = layers[hiddenLayersNumber + 1].neurons[i].value;
         }
+
         return  outputs;
     }
 }
